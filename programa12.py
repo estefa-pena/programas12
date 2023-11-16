50a90f3c0d0120bd1005680f2edaccb37a18a56c
estefa-pena <147632829+estefa-pena@users.noreply.github.com>
2023-11-16 10:24:04 -0500

    Create programa12.py
    se agrego el 12 programa

---
+++ programa12.py
@@ -0,0 +1,6 @@
+#Programa para verificar si un número es par o impar.
+numero = int(input("Ingrese un número: "))
+if numero % 2 == 0:
+    print(f"{numero} es un número par.")
+else:
+    print(f"{numero} es un número impar.")
