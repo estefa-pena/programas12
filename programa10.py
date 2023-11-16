494a73daef303bd1769696b8db5d3026fae15052
estefa-pena <147632829+estefa-pena@users.noreply.github.com>
2023-11-16 10:22:48 -0500

    Create programa10.py
    se agrego el 10 programa

---
+++ programa10.py
@@ -0,0 +1,5 @@
+#Programa para encontrar el máximo entre dos números.
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+maximo = max(numero1, numero2)
+print("El máximo entre", numero1, "y", numero2, "es:", maximo)
