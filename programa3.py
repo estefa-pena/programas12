215b5225643b9a54a6863b4438b92341e456d1a4
estefa-pena <147632829+estefa-pena@users.noreply.github.com>
2023-11-16 10:17:57 -0500

    Create programa3.py
    se agrego el 3 proyecto

---
+++ programa3.py
@@ -0,0 +1,8 @@
+#Programa para verificar si un número es negativo, positivo o cero.
+numero = float(input("Ingresa un número: "))
+if numero < 0:
+    print("El número ingresado es negativo.")
+elif numero > 0:
+    print("El número ingresado es positivo.")
+else:
+    print("El número ingresado es cero.")
