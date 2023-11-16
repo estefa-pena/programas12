bf4e3324816c0a133414439a5dd6aa705b3e0a15
estefa-pena <147632829+estefa-pena@users.noreply.github.com>
2023-11-16 10:15:34 -0500

    Update programa1.py
    agregue el programa 1

--- programa1.py
+++ programa1.py
@@ -1,1 +1,9 @@
-#programa que 
+ #Programa para verificar si un caracter está en mayúsculas o en minúscula.
+Letra = input("Ingrese un carácter: ")
+if 'a' <= Letra <= 'z':
+    print(f"'{Letra}' está en minúsculas.")
+elif 'A' <= Letra <= 'Z':
+    print(f"'{Letra}' está en mayúsculas.")
+else:
+    print(f"'{Letra}' no es una letra.")
+  
