a636fdf4ba67c026a6436113f08626191c3be09b
estefa-pena <147632829+estefa-pena@users.noreply.github.com>
2023-11-16 10:23:30 -0500

    Create programa11.py
    se agrego el 11 programa

---
+++ programa11.py
@@ -0,0 +1,6 @@
+#Programa para encontrar el máximo entre tres números.
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+numero3 = float(input("Ingrese el tercer número: "))
+maximo = max(numero1, numero2, numero3)
+print(f"El máximo entre {numero1}, {numero2} y {numero3} es: {maximo}")
